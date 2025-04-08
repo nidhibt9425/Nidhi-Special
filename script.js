7cb5de9550abb46c799886c35a54b218a7cb5733
Nidhi Singh <nidhibt9468@gmail.com>
2025-04-08 21:44:28 +0530

    Update script.js
    Upadte

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ document.addEventListener('DOMContentLoaded', () => {
   backToTopBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
+  const toggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
 
   // Smooth scrolling for in-page anchors
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
